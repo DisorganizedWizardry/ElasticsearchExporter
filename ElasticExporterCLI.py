9586ab771c5bd3104201f7dfedcaf1ab561a548b
DisorganizedWizardry <93408280+DisorganizedWizardry@users.noreply.github.com>
2023-10-14 22:13:14 -0600

    Update ElasticExporterCLI.py
    - added multiple indices options
    - added help / info

--- ElasticExporterCLI.py
+++ ElasticExporterCLI.py
@@ -2,7 +2,17 @@
 Download an elasticsearch index to ndjson using a PIT search
 
 Usage:
-  ElasticExportCLI.py --index=<indexname> --backup-folder=<backup_folder> [--query-file=<query-file>] [--export-csv]
+  ElasticExportCLI.py --index=<indexname> [--multiple-indexes] [--backup-folder=<backup_folder>] [--query-file=<query_file>] [--export-csv]
+
+-Options:
+  --index=<indexname>  Set the index to export
+  --multiple-indexes   Export multiple indexes at once. use a wildcard for --index=
+                       e.g. --index=logstash*
+  --backup-folder=<backup_folder>
+                       Sets the folder to save the export to
+  --query-file=<query_file>
+                       Sets a query filter to limit what is exported 
+  --export-csv         Also convert the json file to csv.
 """
 
 from elasticsearch import Elasticsearch
@@ -55,7 +65,16 @@ def main():
 
   if settings.get('debug'):
     print (settings)
+    
+  if options.get('--multiple-indexes'):
+    ElasticExporter.ProcessMultipleIndexes(settings)
+    return
 
+  if '*' in settings['index_name']:
+    print ("Found wildcard in index name.")
+    print ("Use --multiple-indexes to export multiple indexes")
+    return
+  
   ElasticExporter.ProcessIndex(settings)
 
 
